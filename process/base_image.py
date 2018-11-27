6e0d77f5781ed9d891a6623aff4295fc54a96dc2
Cheer.L <1017801883@qq.com>
2018-11-27 17:28:11 +0800

    11.27

--- process/base_image.py
+++ process/base_image.py
@@ -4,6 +4,12 @@ import numpy as np
 
 class ImageProcess(object):
     @staticmethod
+    def get_mid_silces(nii, num, dim=0):
+        dim_size = nii.size[dim]
+        start, end = int((dim_size - num) / 2), int((dim_size + num) / 2)
+        return nii.get_slice(start, end, dim)
+
+    @staticmethod
     def get_otsu(img, only_otsu=True):
         otsu, otsu_img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         if only_otsu:
@@ -36,7 +42,7 @@ class ImageProcess(object):
     def get_connected_component(img, min_area=0, sort=True):
         _, label, stats, centroids = cv2.connectedComponentsWithStats(img)
         info = [
-            (level, stat, centroid)
+            (level, stat, ImageProcess.get_revese_point(centroid.astype(np.int)))
             for level, (stat, centroid) in enumerate(zip(stats, centroids))
             if stat[4] > min_area and level != 0
         ]
@@ -57,16 +63,29 @@ class ImageProcess(object):
     @staticmethod
     def add_points(img, points, size=5, color=127, reverse=True):
         def is_point(point):
-            return (isinstance(point, (tuple, list, np.ndarray)) and
-                    len(point) == 2 and
-                    isinstance(point[0], int) and
-                    isinstance(point[1], int))
+            try:
+                if (
+                        isinstance(point, (tuple, list, np.ndarray)) and
+                        len(point) == 2 and (
+                            isinstance(point[0], int) or
+                            point[0].dtype in (np.int, np.int16, np.int32, np.int64)
+                        ) and (
+                            isinstance(point[1], int) or
+                            point[1].dtype in (np.int, np.int16, np.int32, np.int64)
+                        )
+                ):
+                    return True
+            except AttributeError:
+                pass
+            return False
 
         if is_point(points):
             points = [points]
         else:
             for point in points:
                 assert is_point(point)
+        if img.dtype == bool:
+            img = img.astype(np.uint8) * 255
 
         background = img.copy()
         for point in points:
@@ -109,6 +128,26 @@ class ImageProcess(object):
             return -1 * signed * angle_x, signed * angle_y
 
     @staticmethod
+    def get_side_contour(src, side='r'):
+        img = src.copy()
+        height, width = img.shape
+        if side == 'r':
+            for y in range(height):
+                for x in range(width):
+                    if img[y, x:].any():
+                        img[y, x] = img.max()
+                    else:
+                        break
+        elif side == 'l':
+            for y in range(height):
+                for x in range(width-1, 0, -1):
+                    if img[y, :x].any():
+                        img[y, x] = img.max()
+                    else:
+                        break
+        return img
+
+    @staticmethod
     def get_point_by_angle_and_distance(point, angle, distance):
         point_y, point_x = point
         angle_y, angle_x = angle
@@ -116,3 +155,15 @@ class ImageProcess(object):
         diff_x = distance / angle_distance * angle_x
         diff_y = distance / angle_distance * angle_y
         return (int(point_y + diff_y), int(point_x + diff_x))
+
+    @staticmethod
+    def get_region_mask(shape, point, height, width, angle):
+        canvas_height, canvas_width = shape
+        canvas = np.zeros((canvas_height, canvas_width))
+        start_point = ImageProcess.get_revese_point(point)
+        end_point = (point[1] + width, point[0] + height)
+        rect = cv2.rectangle(canvas, start_point, end_point, 255, -1)
+        rot_matrix = cv2.getRotationMatrix2D(start_point, angle, 1)
+        rot_shape = (canvas_width, canvas_height)
+        rect = cv2.warpAffine(rect, rot_matrix, rot_shape, flags=cv2.INTER_LINEAR)
+        return rect.astype(bool)
--- process/stage.py
+++ process/stage.py
@@ -5,12 +5,8 @@ import numpy as np
 from process.base_image import ImageProcess
 
 class FirstStageProcess(ImageProcess):
-    def get_slices_1(self, nii, num, dim=0):
-        return nii.get_slice(
-            int((nii.size[dim]-num)/2),
-            int((nii.size[dim]+num)/2),
-            dim
-        )
+    def get_slices_1(self, nii, num=10, dim=0):
+        return self.get_mid_silces(nii, num, dim)
 
     def get_center_component(self, label, info, rate=1/4):
         height, width = label.shape
@@ -27,12 +23,22 @@ class FirstStageProcess(ImageProcess):
             )
         ]
 
-    def get_near_component(self, label, info, point, limit):
-        return [
-            (level, stat, centroid)
-            for level, stat, centroid in info
-            if self.get_area_distance(label == level, point) < limit
-        ]
+    def get_near_component(self, label, info, pos, limit, type_='point'):
+        if type_ == 'point':
+            near_component = [
+                (level, stat, centroid)
+                for level, stat, centroid in info
+                if self.get_area_distance(label == level, pos) < limit
+            ]
+        elif type_ == 'area':
+            near_component = [
+                (level, stat, centroid)
+                for level, stat, centroid in info
+                if self.get_area_distance(pos, centroid) < limit
+            ]
+        else:
+            raise TypeError()
+        return near_component
 
     def get_corpus(self, img, clahe_limit=0.02, clahe_row=8, clahe_col=8,
                    clahe_bin_add=0.2, center_rate=1/4,
@@ -87,28 +93,36 @@ class FirstStageProcess(ImageProcess):
         num, component = sorted_component_list[0]
         return component, num
 
-    def get_component_bound(self, component, info):
-        y_pos, x_pos = np.where(component)
-        left_x, right_x = int(info[1][0]), int(info[1][0] + info[1][2] - 1)
-        left_y = int(y_pos[np.where(x_pos == left_x)[0]].mean())
-        right_y = int(y_pos[np.where(x_pos == right_x)[0]].mean())
-        return (left_y, left_x), (right_y, right_x)
+    def get_component_bound(self, component, info, type_='lr'):
+        if type_ == 'lr':
+            y_pos, x_pos = np.where(component)
+            left_x, right_x = int(info[1][0]), int(info[1][0] + info[1][2] - 1)
+            left_y = int(y_pos[np.where(x_pos == left_x)[0]].mean())
+            right_y = int(y_pos[np.where(x_pos == right_x)[0]].mean())
+            return (left_y, left_x), (right_y, right_x)
+        elif type_ == 'ud':
+            y_pos, x_pos = np.where(component)
+            up_y, down_y = int(info[1][1]), int(info[1][1] + info[1][3] - 1)
+            up_x = int(x_pos[np.where(y_pos == up_y)[0]].mean())
+            down_x = int(x_pos[np.where(y_pos == down_y)[0]].mean())
+            return (up_y, up_x), (down_y, down_x)
+        elif type_ == 'all':
+            left_bound, right_bound = self.get_component_bound(component, info, 'lr')
+            up_bound, down_bound = self.get_component_bound(component, info, 'ud')
+            return left_bound, right_bound, up_bound, down_bound
 
-    def get_region_1(self, component, info, angle_negative=False, rate=2/3):
-        left_bound, right_bound = self.get_component_bound(component, info)
-        angle = self.get_vert_angle(left_bound, right_bound, angle_negative)
-        distance = self.get_distance(left_bound, right_bound)
-        vert_distance = distance * rate
-        left_vert_bound = self.get_point_by_angle_and_distance(left_bound, angle, vert_distance)
-        right_vert_bound = self.get_point_by_angle_and_distance(right_bound, angle, vert_distance)
-        return left_bound, right_bound, right_vert_bound, left_vert_bound
-
-    def get_region_1_mask(self, img, corpus, info, angle_negative=False, rate=2/3, reverse=True):
-        region_1 = self.get_region_1(corpus, info, angle_negative, rate)
-        if reverse:
-            region_1 = [self.get_revese_point(point) for point in region_1]
-        canvas = np.zeros(img.shape)
-        return cv2.fillPoly(canvas, [np.array(region_1)], 255).astype(bool)
+    def get_region_1_mask(self, component, info, rate=2/3, upside=True):
+        left_bound, right_bound = self.get_component_bound(component, info, 'lr')
+        width = int(self.get_distance(left_bound, right_bound))
+        height = int(width * rate)
+        diff_y, diff_x = right_bound[0] - left_bound[0], right_bound[1] - left_bound[1]
+        angle = -np.rad2deg(np.arctan(diff_y / diff_x))
+        return self.get_region_mask(
+            component.shape,
+            right_bound if upside else left_bound,
+            height, width,
+            180 + angle if upside else angle
+            )
 
     def get_upper_brainstem(self, img, region_1_mask, rate=2.2, test=False):
         region_1_img = img.copy()
@@ -135,9 +149,13 @@ class FirstStageProcess(ImageProcess):
             return upper_brainstem, upper_brainstem_info, test_data
         return upper_brainstem, upper_brainstem_info
 
+    def get_midsagittal(self, upper_brainstem_list, slices_1):
+        upper_brainstem, num = self.get_smallest_component(upper_brainstem_list)
+        return upper_brainstem, slices_1[num]
+
     def get_brainstem(self, img, upper_brainstem,
                       clahe_limit=0.02, clahe_row=8, clahe_col=8,
-                      clahe_bin_add=0.2, min_area=200, max_distance=1,
+                      clahe_bin_add=0.15, min_area=500, max_distance=5,
                       test=False):
         center = self.get_grav_center(upper_brainstem.astype(np.uint8))
         clahe_img = self.get_clahe_image(img, clahe_limit, clahe_row, clahe_col)
@@ -163,3 +181,64 @@ class FirstStageProcess(ImageProcess):
             }
             return brainstem, brainstem_info, test_data
         return brainstem, brainstem_info
+
+    def get_quadrigeminal(self, img, brainstem, region_1_mask,
+                          clahe_limit=0.02, clahe_row=8, clahe_col=8,
+                          clahe_bin_add=0.15, min_area=25, max_distance=5,
+                          test=False):
+        # center = self.get_grav_center(upper_brainstem.astype(np.uint8))
+        clahe_img = self.get_clahe_image(img, clahe_limit, clahe_row, clahe_col)
+        clahe_otsu = self.get_otsu(clahe_img)
+        clahe_bin_img = self.get_binary_image(clahe_img, clahe_otsu + 255 * clahe_bin_add)
+        clahe_bin_img[brainstem] = 0
+        clahe_bin_img[~region_1_mask] = 0
+        label, info = self.get_connected_component(clahe_bin_img, min_area)
+        info = self.get_near_component(label, info, brainstem, max_distance, type_='area')
+        try:
+            quadrigeminal_info = info[0]
+            quadrigeminal = label == quadrigeminal_info[0]
+        except IndexError:
+            quadrigeminal_info = info
+            quadrigeminal = label
+
+        if test:
+            test_data = {
+                'clahe_img': clahe_img,
+                'clahe_bin_img': clahe_bin_img,
+                'clahe_otsu': clahe_otsu,
+                'label': label,
+                'info': info
+            }
+            return quadrigeminal, quadrigeminal_info, test_data
+        return quadrigeminal, quadrigeminal_info
+
+    def get_brainstem_seg_point(self, brainstem, brainstem_info,
+                                size=3, max_num=5, quality=0.5, min_dis=10,
+                                min_dis_from_bound=5):
+        img = brainstem.astype(np.uint8) * 255
+        kernel = np.ones((size, size))
+        img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+        img = self.get_side_contour(img, 'r')
+        corners = cv2.goodFeaturesToTrack(img, max_num, quality, min_dis).astype(np.int)
+        corners = [self.get_revese_point(point) for point in np.concatenate(corners)]
+        brainstem_size_info = brainstem_info[1]
+        left, up = brainstem_size_info[0], brainstem_size_info[1]
+        right, down = left + brainstem_size_info[2] - 1, up + brainstem_size_info[3] - 1
+
+        corners = [
+            point for point in corners
+            if (
+                left + min_dis_from_bound < point[1] < right - min_dis_from_bound and
+                up + min_dis_from_bound < point[0] < down - min_dis_from_bound
+            )
+        ]
+        assert len(corners) == 2
+        return sorted(corners, key=lambda x: x[0])
+
+
+class SecendStageProcess(ImageProcess):
+    def get_region_2_mask(self, shape, point, angle, height=50, width=70, ):
+        return self.get_region_mask(shape, point, height, width, angle)
+
+    def get_silces_2(self, nii, num=30, dim=0):
+        return self.get_mid_silces(nii, num, dim)
