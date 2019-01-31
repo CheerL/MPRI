d10e619a4751918acc82ef8a02fc916836826b7f
Cheer.L <1017801883@qq.com>
2019-01-31 19:11:21 +0800

    change

--- process/second_stage.py
+++ process/second_stage.py
@@ -32,88 +32,17 @@ def _get_mcp_down_point(mcp: component.ConnectedComponent, up_point: Point, max_
     ]
     distance.sort(key=lambda x: x[1])
     return distance[0][0]
-    # allow_right = True
-    # allow_left = True
 
-    # y_count = 0
-    # x_count = 0
-    # max_y_count = 3
-    # max_x_count = 3
-    # right = mcp.right
-    # left = mcp.left
-    # up = mcp.up
-    # op_list = []
-
-    # y, _ = mcp.get_bound_point('d')
-    # pos = np.where(mcp.img[y])[0]
-    # if pos.max() < right:
-    #     x = pos.max() + 1
-    # elif pos.min() > left:
-    #     x = pos.min() - 1
-    # else:
-    #     raise IndexError('No start point')
-
-    # try:
-    #     while True:
-    #         # if y_count < max_y_count and not mcp.img[y-1, x]:
-    #         #     y -= 1
-    #         #     y_count += 1
-    #         # elif mcp.img[y-1, x]:
-    #         #     y_count = 0
-    #         #     x_right = np.where(mcp.img[y-1, x:]==False)[0].min() + x
-    #         #     x_left = np.where(mcp.img[y-1, :x]==False)[0].max()
-    #         #     if x_right < right and x_right - x < max_x_count:
-    #         #         if not mcp.img[y, x:x_right+1]:
-    #         #             x = x_right
-    #         #             y -= 1
-    #         #             continue
-    #         #     if 
-    #         # op_list.append((y, x))
-    #         # if mcp.img[y, x] and x < right and allow_right:
-    #         #     x += 1
-    #         # elif mcp.img[y, x] and x >= right:
-    #         #     allow_right = False
-    #         #     x -= 1
-    #         # elif mcp.img[y, x] and not allow_right:
-    #         #     x -= 1
-    #         # else:
-    #         #     if not mcp.img[y-1, x] and y > up:
-    #         #         y -= 1
-    #         #         y_count += 1
-    #         #         x_count = 0
-    #         #     elif y <= up:
-    #         #         raise IndexError('Come to the top')
-    #         #     else:
-    #         #         if y_count >= max_y_count:
-    #         #             if mcp.img[y, x-1]:
-    #         #                 return y, x
-    #         #             elif allow_left:
-    #         #                 x -= 1
-    #         #                 x_count += 1
-    #         #             elif not allow_left
-    #         #         else:
-    #         #             if allow_right:
-    #         #                 y_count = 0
-    #         #             if x >= right or x_count >= max_x_count:
-    #         #                 allow_right = False
-    #         #                 x_count = 0
-    #         #             if allow_right and not mcp.img[y, x+1]:
-    #         #                 x += 1
-    #         #                 x_count += 1
-    #         #             elif not allow_right and not mcp.img[y, x-1]:
-    #         #                 x -= 1
-    #         #             else:
-    #         #                 return y, x
-    # except:
-    #     process.show([
-    #         mcp.img_uint8[120:160, 120:150],
-    #         process.add_points(
-    #             mcp.img_uint8,
-    #             op_list[:50],
-    #             0, 127
-    #         )[120:160, 120:150]
-    #     ])
-    #     raise EnvironmentError()
+def _get_left_down(image: np.ndarray, down_point: Point) -> int:
+    left_part = image[down_point[0]:, :down_point[1]]
+    for x in range(left_part.shape[1]):
+        if left_part[:, x].any():
+            for y in range(left_part.shape[0]):
+                if left_part[y, x] == False:
+                    left_part[y+1:, x] = False
+                    break
+    left_pos = np.where(left_part)[0]
+    return left_pos.max() if left_pos.any() else 0
 
 def is_mcp_slice(
     label: np.ndarray, medulla_label: int=27, scp_label: int=28,
@@ -125,7 +54,7 @@ def is_mcp_slice(
 
 def get_mcp_seg_point(
     image: np.ndarray, label: np.ndarray, quad_seg_point: Point,
-    rate: float=0.8, pons_label: int=26, box: Box=((10, 40), (-8, 8))
+    rate: float=0.8, pons_label: int=26, box: Box=((10, 40), (-8, 10))
 ) -> Mcp_seg_result:
     pons = label == pons_label
     pons_pos = np.where(pons)
@@ -151,7 +80,7 @@ def run(
     image_nii: RotatedNiiFileManager, label_nii: LabelNiiFileManager, quad_seg_point: Point, mid_num: int,
     pons_label: int=26, medulla_label: int=27, scp_label: int=28, num: int=14, rate: float=0.82,
     max_medulla_vol: int=150, max_scp_vol: int=0, min_dis: int=3, max_dis: int=13,
-    box: Box=((10, 40), (-8, 8)), show: bool=False
+    box: Box=((10, 40), (-8, 10)), show: bool=False
 ) -> Tuple[int, List[Mcp_show_info_item]]:
     mcp_widths = []
     show_info = []
@@ -169,14 +98,11 @@ def run(
                     rate=rate, pons_label=pons_label, box=box
                 )
             except IndexError as error:
-                print(error)
                 continue
 
-            left_part = mcp.img_bool.copy()
-            left_part[:, down_point[1]:] = False
-            mcp_down = np.where(left_part)[0].max()
-            # print(index, mcp_down-down_point[0], mcp.down - down_point[0])
-            if min_dis <= mcp_down - down_point[0] <= max_dis and mcp.down - down_point[0] < 20:
+            mcp_left_down = _get_left_down(mcp.img, down_point)
+
+            if min_dis <= mcp_left_down <= max_dis and mcp.down - down_point[0] < 20:
                 mcp_widths.append(get_mcp_width(up_point, down_point))
                 if show:
                     show_info.append((mid_num-num+index, (up_point, down_point, mcp)))
--- process/third_stage.py
+++ process/third_stage.py
@@ -8,21 +8,44 @@ import process
 from config import Box, Point, Scp_components, Scp_show_info_item
 from DM.file_manage import LabelNiiFileManager, RotatedNiiFileManager
 
+def _get_peak(image: np.ndarray, start: int, end: int) -> Tuple[int, int]:
+    direct = 1 if start < end else -1
+    min_y = 200
+    min_x = start
+    for x in range(start, end, direct):
+        y = (np.where(image[:, x]))[0].min()
+        if y < min_y:
+            count = 0
+            min_y = y
+            min_x = x
+        elif count < 3:
+            count += 1
+        else:
+            break
+    return min_y, min_x
+    # left_peak_list = [
+    #     (np.where(image[:, x])[0].min(), x)
+    #     for x in range(start, end)
+    # ]
+    # left_peak_list.sort(key=lambda x: x[0])
+    # return left_peak_list[0]
 
 def get_scp_sum(label_nii: LabelNiiFileManager, quad_seg_point: Point, scp_label: int=28) -> int:
     quad_num = int(quad_seg_point[1])
 
-    for num in range(quad_num-3, quad_num+5):
+    for num in range(quad_num, quad_num+5):
         label = label_nii.get_slice(num, dim=1)
         scp = label==scp_label
         if scp.sum():
-            return num-1
-    # return quad_num + 1
+            return num
+    # return quad_num
 
 def get_scp_components(
     image: np.ndarray, label: np.ndarray, quad_point: Point,
-    scp_label: int=28, pons_label: int=26, rate: float=0.95, box: Box=((2, 13), (-10, 10))
+    scp_label: int=28, pons_label: int=26, rate: float=0.92,
+    box: Box=((3, 13), (-10, 10))
 ) -> Scp_components:
+    # print(quad_point)
     mask_box = (
         slice(quad_point[0]+box[0][0], quad_point[0]+box[0][1]),
         slice(quad_point[1]+box[1][0], quad_point[1]+box[1][1])
@@ -30,45 +53,67 @@ def get_scp_components(
     mask = np.zeros(image.shape, bool)
     mask[mask_box] = True
     cere_pon_mask = np.zeros(image.shape, bool)
-    cere_pon_mask[quad_point[0]:quad_point[0]+50, quad_point[1]-40:quad_point[1]+40] = True
+    cere_pon_mask[
+        quad_point[0]+box[0][0]:quad_point[0]+50,
+        quad_point[1]-40:quad_point[1]+40
+    ] = True
     scp = np.logical_or(label==scp_label, label==pons_label)
     scp_mean = image[np.where(scp*mask)].mean()
     bin_image = process.clear_bottleneck(image * cere_pon_mask > scp_mean * rate)
     cere_pon = component.get_connected_component(bin_image)[0][0]
 
-    # center_min_y = np.where(cere_pon.img[:, quad_point[1]])[0].min()
-    left_peak_list = [
-        (np.where(cere_pon.img[:, x])[0].min(), x)
-        for x in range(quad_point[1]-10, quad_point[1]-2)
-    ]
-    left_peak_list.sort(key=lambda x: x[0])
-    left_peak_y, left_peak_x = left_peak_list[0]
-    y, x = left_peak_y-1, left_peak_x
+    left_peak_y, left_peak_x = _get_peak(
+        cere_pon.img,
+        quad_point[1]-1,
+        quad_point[1]-10
+    )
 
+    right_peak_y, right_peak_x = _get_peak(
+        cere_pon.img,
+        quad_point[1]+1,
+        quad_point[1]+10
+    )
+    mean_peak = (
+        int((left_peak_y + right_peak_y) / 2),
+        int((left_peak_x + right_peak_x) / 2)
+    )
+
+    y, x = left_peak_y-1, left_peak_x
     while x > left_peak_x - 15:
         if not cere_pon.img[y+1, x]:
             y += 1
         elif not cere_pon.img[y, x-1]:
             x -= 1
         else:
-            break
+            corner = cere_pon.img[y+1:y+4, x-2:x+1]
+            if corner.all():
+                break
+            else:
+                false_pos = np.where(corner==False)
+                _y = false_pos[0].min()
+                _x = 2 - false_pos[1].max()
+                cere_pon.img[y, x-_x:x] = False
+                cere_pon.img[y:y+_y+1, x-_x] = False
+
     left_y, left_x = y, x
 
-    right_peak_list = [
-        (np.where(cere_pon.img[:, x])[0].min(), x)
-        for x in range(quad_point[1]+2, quad_point[1]+10)
-    ]
-    right_peak_list.sort(key=lambda x: x[0])
-    right_peak_y, right_peak_x = right_peak_list[0]
     y, x = right_peak_y-1, right_peak_x
-
     while x < right_peak_x + 15:
         if not cere_pon.img[y+1, x]:
             y += 1
         elif not cere_pon.img[y, x+1]:
             x += 1
         else:
-            break
+            corner = cere_pon.img[y+1:y+4, x:x+3]
+            if corner.all():
+                break
+            else:
+                false_pos = np.where(corner==False)
+                _y = false_pos[0].min()
+                _x = false_pos[1].max()
+                cere_pon.img[y, x:x+_x+1] = False
+                cere_pon.img[y:y+_y+1, x+_x] = False
+
     right_y, right_x = y, x
 
     for y in range(min(right_y, left_y), max(right_peak_y, left_peak_y), -1):
@@ -81,15 +126,21 @@ def get_scp_components(
             center_row_seg = row_seg_list[0]
             if abs(center_row_seg.mean() - quad_point[1]) < 5:
                 center_y = y
-                center_x = int(center_row_seg.mean())
                 break
 
     cere_pon_image_mask = np.zeros(image.shape, bool)
-    cere_pon_image_mask[:min(left_y, right_y)+1, left_x:right_x] = True
+    cere_pon_image_mask[:min(left_y, right_y), left_x:right_x] = True
     cere_pon_image = cere_pon.img_bool * cere_pon_image_mask
     components = component.get_connected_component(cere_pon_image, 10)[0]
     big_component = components[0]
-    process.show([bin_image, cere_pon, cere_pon_image] + components)
+    # process.show([bin_image, cere_pon, cere_pon_image, big_component,
+    # process.add_points(cere_pon_image,
+    # [
+    #     (right_peak_y, right_peak_x),
+    #     (left_peak_y, left_peak_x),
+    #     quad_point
+    # ], size=2, color=127
+    # )])
     if big_component.right - big_component.left > 16:
         big_component.img[
             center_y-1:,
@@ -98,16 +149,24 @@ def get_scp_components(
         scp_components, _ = component.get_connected_component(
             big_component.img_uint8
         )
-        process.show(scp_components)
     else:
+        components.sort(key=lambda x: process.get_area_distance(x.img, mean_peak))
         scp_components = components[:2]
-        # process.show(scp_components + [image, _bin_scp, cere_pon])
     assert len(scp_components) == 2
+
+    for scp_component in scp_components:
+        scp_component_row_sum = scp_component.img.sum(1)
+        row_diff = np.convolve(scp_component_row_sum, [1, -0.5, -0.5])
+        big_diff = np.where(row_diff > 3)[0]
+        big_diff = big_diff[big_diff > scp_component.down - 5]
+        if big_diff.any():
+            scp_component.img[big_diff[0]:, :] = False
+    # process.show(scp_components)
     return scp_components
 
 def get_scp_width(
     component: component.ConnectedComponent,
-    min_width: int=2, max_width: int=10
+    min_width: int=2, max_width: int=12
 ) -> int:
     contours = cv2.findContours(
         component.img_uint8,
@@ -122,20 +181,16 @@ def get_scp_width(
         process.get_distance(box[0], box[2]),
         process.get_distance(box[0], box[3])
     )
+    # print(box)
     assert width < max_width
     assert min_width < width
-    # if test:
-    #     test_data = {
-    #         'box': np.around(box).astype(np.int),
-    #     }
-    #     return width, test_data
     return width
 
 def run(
     image_nii: RotatedNiiFileManager, label_nii: LabelNiiFileManager,
     quad_seg_point: Point, mid_num: int,
-    num: int=2, scp_label: int=28, pons_label: int=26, rate: float=0.95,
-    max_width: int=10, min_width: int=2, box: Box=((3, 13), (-10, 10)), show: bool=False
+    num: int=2, scp_label: int=28, pons_label: int=26, rate: float=0.92,
+    max_width: int=12, min_width: int=2, box: Box=((3, 13), (-10, 10)), show: bool=False
 ) -> Tuple[float, List[Scp_show_info_item]]:
     scp_num = get_scp_sum(label_nii, quad_seg_point, scp_label=scp_label)
     quad_point = (quad_seg_point[0], mid_num)
