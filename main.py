0342b481b92a5969b5a3cf9082d1c7bb03e3a1f6
Cheer.L <1017801883@qq.com>
2019-04-02 14:41:05 +0800

    readme

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from DM.file_manage import RotatedNiiFileManager, LabelNiiFileManager
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('func', choices=['calc', 'seg', 'both'])
+    parser.add_argument('mode', choices=['calc', 'seg', 'both'])
     parser.add_argument('-i', '--image', required=True)
     parser.add_argument('-l', '--label', required=True)
     parser.add_argument('-o', '--output')
     parser.add_argument('-d', '--data')
-    parser.add_argument('-s', '--show', action='store_true')
     args = parser.parse_args()
     assert os.path.isfile(args.label)
     assert os.path.isfile(args.image)
@@ -21,34 +20,30 @@ if __name__ == "__main__":
 
     label_nii = LabelNiiFileManager(args.label)
     label_nii.load()
-
+    show = args.mode != 'calc'
     quad_seg_point, mid_num, pons_area, midbrain_area = process.first_stage.run(
         image_nii, label_nii
     )
     mcp_mean_width, mcp_show_info = process.second_stage.run(
-        image_nii, label_nii, quad_seg_point, mid_num, show=args.show
+        image_nii, label_nii, quad_seg_point, mid_num, show
     )
     scp_mean_width, scp_show_info = process.third_stage.run(
-        image_nii, label_nii, quad_seg_point, mid_num, show=args.show
+        image_nii, label_nii, quad_seg_point, mid_num, show
     )
-    if args.func == 'calc':
+    if args.mode == 'calc':
         process.output.calc(pons_area, midbrain_area, mcp_mean_width, scp_mean_width, file_name=args.data)
-    elif args.func == 'seg':
+    elif args.mode == 'seg':
         assert args.output
-        assert args.show
         process.output.save_to_sitk(
             args.output, image_nii.size, mid_num,
             quad_seg_point, mcp_show_info, scp_show_info
         )
-    elif args.func == 'both':
+    elif args.mode == 'both':
         assert args.output
-        assert args.show
         process.output.save_to_sitk(
             args.output, image_nii.size, mid_num,
             quad_seg_point, mcp_show_info, scp_show_info
         )
         process.output.calc(pons_area, midbrain_area, mcp_mean_width, scp_mean_width, file_name=args.data)
-    elif args.func == 'pic':
-        raise NotImplementedError('`pic` have not been implemented')
     else:
-        raise TypeError('func must be one of {calc, seg, both}')
+        raise TypeError('mode must be one of {calc, seg, both}')
